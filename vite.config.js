acdde023b3540f0179815715d18ce126e2a64b2d
Julia K <yy.caramella@gmail.com>
2026-05-31 02:01:00 +0300

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: command === 'build' ? '/goit-js-hw-12/' : '/',
+    base: '/goit-js-hw-12/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
